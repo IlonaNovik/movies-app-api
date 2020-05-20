0ecc43d2baf42ad14f4e9a48d5cb048323307883
IlonaNovik <ilonanovik1711@gmail.com>
2020-05-20 15:56:17 +0200

    Push heroku

--- app/settings.py
+++ app/settings.py
@@ -76,7 +76,6 @@ DATABASES = {
         'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
-
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
